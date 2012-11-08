4a7a95e66855631db0235eb3294de91d5fcd354d
Chris Myers <christopherbm@gmail.com>
2012-11-08 10:50:10 -0800

    euler1.py fixed for speed

--- euler1.py
+++ euler1.py
@@ -1,34 +1,15 @@
 """rewrite for speed"""
 def testing():
 	r = range(1, 1000)
-	res_three = []
+	res = []
 	for i in r:
-		m = i % 3
-		if(m == 0):
-			res_three.append(i)
+		if( i % 3 == 0 or i % 5 == 0 ):
+			res.append(i)
 	
-	#print res_three
-	
-	res_five = []
-	for j in r:
-		n = j % 5
-		if(n == 0):
-			res_five.append(j)
-	
-	#print res_five
-	
-	all = set(res_three + res_five)
-	
-	#print all
-	
-	total = 0
-	for a in all:
-		total = total + a
-		
+	total = sum( set(res) )		
 	print total
 
 
 if __name__ == '__main__':
     testing()
     print sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
-    #print returnModuloOfThree(10)
