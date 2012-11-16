ce66b95ac8c6061b13c4be0ca2fbf38d42a1a302
Chris Myers <christopherbm@gmail.com>
2012-11-16 08:19:25 -0800

    euler 3 new logic - prime multiplication

--- euler3.py
+++ euler3.py
@@ -5,19 +5,55 @@ fully tested. Works for n=13195.
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
+import sys
+import math
 
 def euler():
 	n = 600851475143
 	# prime factors: 5, 7, 13, 29
 	#n = 13195
-	answers = []
-	factors = doFactor(n)
-	for f in factors:
-		g = doFactor(f)
-		if len(g) == 0:
-			answers.append(f)
+	
+	p = 5
+	pm = 0
+	finished = False
+	while(finished == False):
+		if( isPrime(p) ):
+			if(p*2 > n):
+				finished = True
 			
-	print answers
+			# on first iteration
+			if(pm == 0):
+				i = 2 
+				while(i != 0):
+					res = p * i
+					if(res == n):
+						# is prime factor
+						print p
+						pm = i
+						i = 0
+					elif(res > n):
+						# not a factor
+						i = 0
+					else:
+						i = i + 1
+			
+			# not first iteration
+			i = pm
+			while(i > 1):
+				res = p * i
+				if(res == n):
+					# is prime factor
+					print p
+					pm = i
+					i = 0
+				elif(res < n):
+					#not a factor
+					i = 0
+				else:
+					i = i - 1
+		
+		# increment p by 2 to keep odd
+		p = p + 2
 		
 def doFactor(n):
 	l = []
@@ -38,6 +74,8 @@ def isPrime(n):
 		if n % s == 0:
 			return False
 		s = s - 1
+		
+	return True
 
 if __name__ == '__main__':
     euler()
